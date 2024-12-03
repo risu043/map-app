8a0ca8a8227da1dd9ee98806bf7c7c96b025082a
ueda mai <uedamai813@gmail.com>
2024-12-03 14:54:53 +0900

    force-dynamic

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,6 +1,8 @@
 import prisma from '../../lib/db';
 import { type NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
   try {
     const page = Number(req.nextUrl.searchParams.get('page') || '1');
--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Suspense } from 'react';
 import { useRouter } from 'next/navigation';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 
@@ -51,46 +50,42 @@ export default function Pagination({
   };
 
   return (
-    <Suspense fallback={<div>...</div>}>
-      <div className="flex space-x-4">
-        <button
-          onClick={handlePrevPageClick}
-          disabled={page === 1}
-          className={
-            page === 1 ? 'text-gray-300' : 'hover:text-gray-400 transition'
-          }
-        >
-          <ChevronLeft className="h-4 w-4" />
-        </button>
+    <div className="flex space-x-4">
+      <button
+        onClick={handlePrevPageClick}
+        disabled={page === 1}
+        className={
+          page === 1 ? 'text-gray-300' : 'hover:text-gray-400 transition'
+        }
+      >
+        <ChevronLeft className="h-4 w-4" />
+      </button>
 
-        <ul className="flex space-x-4">
-          {Array.from({ length: totalPages }, (_, index) => (
-            <li key={index}>
-              {page === index + 1 ? (
-                <span className="block grid place-items-center w-8 h-8 rounded-full bg-blue-500 text-white">
-                  {index + 1}
-                </span>
-              ) : (
-                <button
-                  onClick={() => handleButtonPageClick(index)}
-                  className="w-8 h-8 border rounded-full hover:bg-indigo-50 transition"
-                >
-                  {index + 1}
-                </button>
-              )}
-            </li>
-          ))}
-        </ul>
-        <button
-          onClick={handleNextPageClick}
-          disabled={isLast}
-          className={
-            isLast ? 'text-gray-300' : 'hover:text-gray-400 transition'
-          }
-        >
-          <ChevronRight className="h-4 w-4" />
-        </button>
-      </div>
-    </Suspense>
+      <ul className="flex space-x-4">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <li key={index}>
+            {page === index + 1 ? (
+              <span className="block grid place-items-center w-8 h-8 rounded-full bg-blue-500 text-white">
+                {index + 1}
+              </span>
+            ) : (
+              <button
+                onClick={() => handleButtonPageClick(index)}
+                className="w-8 h-8 border rounded-full hover:bg-indigo-50 transition"
+              >
+                {index + 1}
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={handleNextPageClick}
+        disabled={isLast}
+        className={isLast ? 'text-gray-300' : 'hover:text-gray-400 transition'}
+      >
+        <ChevronRight className="h-4 w-4" />
+      </button>
+    </div>
   );
 }
